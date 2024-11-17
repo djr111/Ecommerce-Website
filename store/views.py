74987f30885f7bc33d4499430c63496a465975d3
Buls Raimonds <djr111@inbox.lv>
2024-11-17 17:14:56 +0000

    added products / cart / checkout to user. Added render to images for all html pages. Added price / quantity update for items selected

--- store/views.py
+++ store/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
+from .models import *
+
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for none-logged in users
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+        
+
+    context = {'items' :items, 'order': order}
     return render (request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render (request, 'store/checkout.html', context)
+     
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for none-logged in users
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+        
+
+    context = {'items':items, 'order':order}
+    return render(request, 'store/checkout.html', context)
 
 
 # Create your views here.
